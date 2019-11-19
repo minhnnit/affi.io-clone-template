1241387cf81ac3122cc2a0402869ea7944ceb31f
minhnnit <nguyennhuminh1208@gmail.com>
2019-11-19 09:18:01 +0700

    create affi.io clone template

---
+++ affi.io clone/js/app.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('.morelink').click(function () {
+        $('.shortcontent').css("display","none");
+        $('.allcontent').css("display","unset");
+    });
+});
